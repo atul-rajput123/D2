f26c9e768bb6883a054863b18113e32515a12fc1
atul-rajput123 <rajputatul2025@gmail.com>
2024-02-27 00:39:14 +0530

    new file

---
+++ Demo1.java
@@ -0,0 +1,16 @@
+class Demo1 {
+        public static void main(String args[]) 
+        {
+            String s[]=new String[10];
+			
+			char c='A';
+			for(;;)
+			{
+				for(int i=c;i<90;i++)
+				{
+					
+				}
+				
+			}
+        } 
+    }
